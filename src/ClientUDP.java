ad112d3a57aeba1edf8ea7567009c3a684d0796b
Nam Nguyen <namnguyen1995@gmail.com>
2016-11-26 20:54:03 -0500

    Not working file transfer

--- src/ClientUDP.java
+++ src/ClientUDP.java
@@ -1,6 +1,61 @@
-/**
- * Created by NamNguyen on 11/26/16.
- */
-public class ClientUDP {
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
 
+
+public class ClientUDP {
+    public static void main(String args[]) throws Exception{
+        DatagramSocket clientSocket = new DatagramSocket();
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter address: ");
+        String address = inFromUser.readLine();
+        System.out.println("Enter port number: ");
+        int port = Integer.parseInt(inFromUser.readLine());
+        boolean on = true;
+
+        while(on){
+            System.out.println("Enter Filename: ");
+            String fileName = inFromUser.readLine();
+            byte[] sendData = fileName.getBytes();
+            InetAddress IPAddress = InetAddress.getByName(address);
+            DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,port);
+            clientSocket.send(sendPacket);
+
+            byte[] receiveData = new byte[50000];
+            byte[] sizeBuffer = new byte[4];
+            System.arraycopy(sizeBuffer, 0, receiveData, 0, 4);
+            int sizeCheck = ByteBuffer.wrap(sizeBuffer).getInt();
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+            clientSocket.receive(receivePacket);
+
+            if (sizeCheck != -1) {
+                byte[] buffer = new byte[1024];
+                FileOutputStream fileIn = new FileOutputStream(fileName);
+                int totalCount = 0;
+                while (totalCount < sizeCheck - 1) {
+                    //Not sure what to do here
+                    int localCount = clientSocket.getReceiveBufferSize();
+                    totalCount = totalCount + localCount;
+                    fileIn.write(buffer, 0, localCount);
+                }
+                // fill buffer with 1024 bytes and save to
+                // file until total bytes read equals file size.
+
+                fileIn.close();
+            } else {
+                System.out.println("File not found");
+
+            }
+
+
+            String messageBack = new String(receiveData);
+            String exit = "Exit";
+            if(fileName.equals(exit)){
+                on = false;
+                break;
+            }
+            System.out.println("Server sent back message: " + messageBack);
+        }
+    }
 }
--- src/ServerUDP.java
+++ src/ServerUDP.java
@@ -1,6 +1,61 @@
-/**
- * Created by NamNguyen on 11/26/16.
- */
+import java.io.*;
+import java.net.*;
+import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+
 public class ServerUDP {
+    private static Map<String, File> scanFolder(String dir) {
+        Map<String, File> folderMap = new HashMap<String, File>();
+        File folder = new File(dir);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                folderMap.put(file.getName(), file);
+            }
+        }
+        return folderMap;
+    }
+
+    public static void main(String args[]) throws Exception{
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        final String DIRECTORY = "src";
+        Map<String, File> fileMap = scanFolder(DIRECTORY);
+
+        while(true){
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+            serverSocket.receive(receivePacket);
+            String requestedFile=  new String(receiveData);
+            System.out.println("Got message: " + requestedFile);
+
+            if (fileMap.containsKey(requestedFile)) {
+                File fileToSend = fileMap.get(requestedFile);
+                // Determine specified file if it exists
+                int fileSize = (int) fileToSend.length();
+                byte[] byteArray = ByteBuffer.allocate(fileSize + 4).putInt(0, fileSize).array();
+                // Create byteArray and allocate enough size for file + 4
+                // bytes to send fileSize.
+                BufferedInputStream fileIn = new BufferedInputStream(new FileInputStream(fileToSend));
+                fileIn.read(byteArray, 4, fileSize);
+                DatagramPacket sendPacket = new DatagramPacket(byteArray,byteArray.length);
+                serverSocket.send(sendPacket);
+                fileIn.close();
+                // Close file InputStream.
+            }
+            else {
+                byte[] byteArray = ByteBuffer.allocate(4).putInt(-1).array();
+                // Signal an error occurred (file not found).
+                // Send byteArray (with error).
+                System.out.println("File not found.");
+            }
+
+        }
+    }
 
 }
